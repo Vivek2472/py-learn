1053cfd9b3b2d822ef8e08276bf4edfbf6de8f14
Vivek2472 <vivekdarbastwar9949@gmail.com>
2026-09-10 19:53:12 +0530

    addition and finding avg

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+# addition of 3 numbers and finding of 3 numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# sum
+sum = a+b+c
+# print sum
+print(f"Sum of {a} + {b} + {c} is: ", sum)
+
+# avg
+avg = sum/3
+# print avg
+print(f"Average of {a},{b},{c} is: ", avg)
+
+
