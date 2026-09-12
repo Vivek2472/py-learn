f6255e0d6e7b6cc594f73fdbb0e5ea6865317e22
Vivek2472 <vivekdarbastwar9949@gmail.com>
2026-09-12 19:00:34 +0530

    Finding area of rectangle

---
+++ rect.py
@@ -0,0 +1,8 @@
+# calculating area of rectangle
+length = float(input("Enter length (in CM): "))
+height = float(input("Enter height (in CM): "))
+
+area = length * height #calculating area of length
+# print
+print("Area of rectangle: ", area, "CM")
+
